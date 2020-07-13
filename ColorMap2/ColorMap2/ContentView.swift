409a78d5088c93f217cf6d011c287b22566589ad
Friedrich Kasper Völkers <friedrich@Friedrichs-MBP.fritz.box>
2020-07-13 19:13:53 +0200

    Adding new version

---
+++ ColorMap2/ColorMap2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorMap2
+//
+//  Created by Friedrich Kasper Völkers on 13.07.20.
+//  Copyright © 2020 Friedrich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
